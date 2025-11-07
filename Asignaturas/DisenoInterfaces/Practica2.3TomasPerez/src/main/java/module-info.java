8eb796e900f907c7fecb36cfc29aec56f52c5b5a
TomasPerezC21 <13788476@alu.murciaeduca.es>
2025-11-07 10:11:38 +0100

    interfaces

---
+++ Asignaturas/DisenoInterfaces/Practica2.3TomasPerez/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module org.tomasperezpractica.practica2tomasperez {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.desktop;
+
+
+    opens org.tomasperezpractica2.practica2tomasperez to javafx.fxml;
+    exports org.tomasperezpractica2.practica2tomasperez;
+}
