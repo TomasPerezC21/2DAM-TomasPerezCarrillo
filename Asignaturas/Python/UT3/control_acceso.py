bdf0989ea9ba4b862fc85d5cf910d63a61168d4b
Tomás <13788476@alu.murciaeduca.es>
2025-11-24 15:51:04 +0100

    Definición de valores y solicitud de datos al usuario

---
+++ Asignaturas/Python/UT3/control_acceso.py
@@ -0,0 +1,13 @@
+import time
+
+# --- 1. CONFIGURACIÓN INICIAL (Valores definidos en el código) ---
+usuario_guardado = "admin"
+contrasena_guardada = "1234"
+sesion_activa = False # Variable para controlar el acceso
+
+print("--- SISTEMA DE LOGIN ---")
+
+# --- 2. PEDIR DATOS Y VERIFICAR ---
+usuario_input = input("Introduce tu usuario: ")
+contrasena_input = input("Introduce tu contraseña: ")
+
